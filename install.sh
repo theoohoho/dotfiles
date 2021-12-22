2209fde2787bcba504f5e0044d67d410e914f11d
theoohoho <topslimer@gmail.com>
2021-12-22 09:13:13 +0800

    build(install.sh): Add installation script for common usage tools

---
+++ install.sh
@@ -0,0 +1,21 @@
+# update apt
+sudo apt-get update
+
+# install trivy
+sudo apt-get install wget apt-transport-https gnupg lsb-release
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get install trivy
+
+# install package
+apt-get install tldr
+
+# install docker
+apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get install docker-ce docker-ce-cli containerd.io
